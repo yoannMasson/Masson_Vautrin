b65b7cb6c766af93be7a8195c7bf82f83ff31a11
yoannMasson <yoann.masson95@gmail.com>
2016-04-17 18:13:17 +0200

    Ajout HighScore Class
    Ajout:
    HighScore Class
    Demande du pseudo
    
    En Attente:
    FilesSample.txt qui devrait être dispo sur Moodle

---
+++ src/scores/TestHighScore.java
@@ -0,0 +1,35 @@
+package scores;
+
+import java.util.*;
+
+/**
+ * 
+ * @author Yoann Masson & Baptiste Vautrin
+ *
+ *	Classe de test de la première version de HighScore
+ */
+public class TestHighScore {
+
+	public static void main(String[] args) {
+
+		String pseudo;
+
+		System.out.println("Rentrez votre pseudo.");
+		Scanner s = new Scanner(System.in);
+		pseudo = s.next();
+
+
+
+
+
+
+
+
+
+
+		s.close();
+
+
+
+	}
+}

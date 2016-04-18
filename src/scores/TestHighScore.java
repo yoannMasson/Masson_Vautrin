32af9f3b5e88d65fea4d2727e14d70ec73caa3e5
yoannMasson <yoann.masson95@gmail.com>
2016-04-18 18:07:21 +0200

    TestHighScore Fini ??
    Ajout:
    Fichier scoreSample ( toujours en attente de celui du prof)
    
    Update:
    TestHighScore Class fini
    Ouverture/lecture du fichier
    Choix aléatoire d'un score
    Affichage du score du joueur
    
    Amelioration à faire:
    Cas où le fichier n'est pas au format donné ??

--- src/scores/TestHighScore.java
+++ src/scores/TestHighScore.java
@@ -1,5 +1,6 @@
 package scores;
 
+import java.io.*;
 import java.util.*;
 
 /**
@@ -11,25 +12,43 @@ import java.util.*;
 public class TestHighScore {
 
 	public static void main(String[] args) {
-
+		String nomFichier = "scoreSamples.txt";
+		FileInputStream file = null;
+		InputStreamReader inputStream = null;
+		List<Integer> scores = new LinkedList<>();
 		String pseudo;
-
-		System.out.println("Rentrez votre pseudo.");
 		Scanner s = new Scanner(System.in);
+
+		System.out.println("Rentrez votre pseudo.");//Demande du pseudo
 		pseudo = s.next();
-
-
-
-
-
-
-
-
-
-
 		s.close();
 
-
-
+		try{
+			file = new FileInputStream(nomFichier);
+			inputStream = new InputStreamReader(file);
+			BufferedReader buffer = new BufferedReader(inputStream);
+			//Remplissage de la liste des scores 
+			LireScoreFichier(scores,buffer);
+			System.out.println("Voici votre score "+pseudo+": "+scores.get((int)(Math.random()*scores.size())));
+			buffer.close();
+			//Gestion des exceptions
+		}catch(FileNotFoundException e){
+			System.out.println("Le fichier "+nomFichier+" n'existe pas");
+			e.printStackTrace();
+		}catch (IOException e) {
+			System.out.println("Problème lors de la lecture du fichier "+nomFichier);
+			e.printStackTrace();
+		}
 	}
+	
+	private static void LireScoreFichier(List<Integer> scores,BufferedReader buffer) throws IOException{
+		String score;
+		score = buffer.readLine();
+		while (score  != null){
+			scores.add(Integer.parseInt(score));
+			score = buffer.readLine();	
+		}
+		
+	}
+
 }

22b75bdf897c6e3751e8e244d0b0a4b141fc2647
vautrinss <baptistevautrin52@gmail.com>
2016-04-26 23:24:01 +0200

    Maj tenBestScores
    Suppression trucs inutiles et tentative d'enlever mes erreurs dans le code

--- src/scores2/HighScore2.java
+++ src/scores2/HighScore2.java
@@ -134,17 +134,6 @@ public class HighScore2 {
 		return tabPlayer;
 	}
 	
-    public static void tribulles(int t[])
-    {
-            for (int i=0 ;i<=(t.length-2);i++)
-                    for (int j=(t.length-1);i < j;j--)
-                            if (t[j] < t[j-1])
-                            {
-                                    int x=t[j-1];
-                                    t[j-1]=t[j];
-                                    t[j]=x;
-                            }
-    } 
 
 	//Classe qui va permettre la connection (voir http://www.mkyong.com/java/how-to-automate-login-a-website-java-example/)
 	private class HttpConnection{
